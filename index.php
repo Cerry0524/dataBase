7526dedaae71fed8ca87284ac562191843eca22f
Cerry <cerry0524@gmail.com>
2023-05-15 09:46:55 +0800

    完成首頁表單設計

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
-<?php
-$dsn="mysql:host=localhost;charset=utf8;dbname=student";//dbname是資料庫名稱不是資料表
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>會員註冊</title>
+</head>
+<body>
+    <h1>會員註冊</h1>
+    <form action="add_user.php" method="post">
+        <p>
+            <label for="">姓名：</label>
+            <input type="text" name="name" id="name">
+        </p>
+        <p>
+            <label for="">帳號：</label>
+            <input type="text" name="acc" id="acc">
+        </p>
+        <p>
+            <label for="">密碼：</label>
+            <input type="password" name="password" id="password">
+        </p>
+        <p>
+            <label for="">電子郵件：</label>
+            <input type="text" name="email" id="email">
+        </p>
+        <p>
+            <label for="tel">電話：</label>
+            <!-- label for只能for id 使用 -->
+            <input type="number" name="tel" id="tel">
+        </p>
+        <p><input type="submit" value="註冊"><input type="reset" value="重置"></p>
+        <!-- 重置跟清除不一樣，如果要清除要另外寫程式 -->
 
-$pdo=new PDO($dsn,'root','');//資料庫設定資料，管理者帳號，密碼
 
-$sql="select * from students limit 1";//用php把資料送到sql執行
-//限制取回的資料型態
-$row=$pdo->query($sql ,PDO::FETCH_NUM)->fetch();//fetch()怎麼拿到資料，寫在前面也可以
-
-echo "<pre>";
-print_r($row);
-echo "</pre>";
-?>
-
-<a href="./cruq/crud.php">CRUD頁面</a>
+    </form>
+    
+</body>
+</html>
